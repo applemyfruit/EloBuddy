efe00d687d4a7072968722a0e455ed22e8333f4f
twicci <twicci@me.com>
2015-11-13 21:22:58 +0100

    oops

--- TBlitzReworked/TBlitzReworked/Program.cs
+++ TBlitzReworked/TBlitzReworked/Program.cs
@@ -12,6 +12,7 @@ namespace TBlitzReworked
 
         private static void OnLoaded(System.EventArgs args)
         {
+            if (ObjectManager.Player.ChampionName == "Blitzcrank")
             new Blitzcrank();
             //Definitely do not check out my AIO as well, AIO's are much illegal :^) You'd get arrested by EB police
         }
